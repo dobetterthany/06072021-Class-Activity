ef6d2d69337c6168be8f63e99823ac0bc4379779
Gyeongjae Kim <kimg009263@gmail.com>
2021-07-06 12:22:42 +1200

    Add project files.

---
+++ 06072021TestingActivity/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06072021TestingActivity
+{
+    
+    public class Mathematics
+    {
+        double result;
+        public double Add(double a, double b)
+        {
+            result = a + b;
+
+            return result;
+        }
+        public double Subtract(double a, double b)
+        {
+            result = a - b;
+
+            return result;
+        }
+        public double Multiply(double a, double b)
+        {
+            result = a * b;
+
+            return result;
+        }
+        public double Divide(double a, double b)
+        {
+            result = a / b;
+
+            return result;
+        }
+
+        public double Test(double a)
+        {
+            return a;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathematics math = new Mathematics();
+
+            Console.WriteLine("Hello!!! Welcome to the option calculator");
+            Console.WriteLine("Choose the option: 1(Add), 2(Multiply), 3(Subtract), 4(Test)");
+
+            int Opt = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type your first number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Type your second number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            switch (Opt)
+            {
+                case 1:
+                    Console.WriteLine("The result is {0}", math.Add(num1, num2));
+                    break;
+                case 2:
+                    Console.WriteLine("The result is {0}", math.Multiply(num1, num2));
+                    break;
+                case 3:
+                    Console.WriteLine("The result is {0}",math.Subtract(num1, num2));
+                    break;
+                case 4:
+                    Console.WriteLine("The result is {0}",math.Test(num2));
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnitTestMathematics/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestMathematics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestMathematics")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8254c8fa-ba02-46b0-921d-4c7061748dec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
